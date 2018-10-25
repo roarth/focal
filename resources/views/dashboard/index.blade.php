47eefcf6603b12ae2898d9b30cfb876d04a9ead2
Mirete Benoit <ben@mirete.fr>
2018-10-25 20:26:30 +0200

    Fix login & add logout link

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
     <dashboard-component></dashboard-component>
+    <br/>
+    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
+        Logout
+    </a>
+    <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+    </form>
 @endsection
